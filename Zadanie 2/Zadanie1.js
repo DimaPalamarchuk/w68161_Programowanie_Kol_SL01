6c42cfe040ed2d8db4581010049b6ad732c9223b
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2023-05-19 14:55:07 +0200

    Kolokwium

--- Zadanie 2/Zadanie1.js
+++ Zadanie 2/Zadanie1.js
@@ -13,3 +13,4 @@ function changeColor(button) {
      }
     }
    }
+
---
+++ Zadanie 2/Zadanie2.js
@@ -0,0 +1,47 @@
+var lights = ['red', 'yellow', 'green'];
+    var currentIndex = 0;
+    
+    function changeColor(element) {
+      var previousIndex = currentIndex;
+      currentIndex = lights.indexOf(element.id);
+      
+      if (currentIndex !== previousIndex) {
+        document.getElementById(lights[previousIndex]).classList.remove(lights[previousIndex]);
+        element.classList.add(lights[currentIndex]);
+      }
+    }
+    
+    function changeLights() {
+      var currentLight = document.getElementById(lights[currentIndex]);
+      currentLight.classList.remove(lights[currentIndex]);
+      
+      currentIndex = (currentIndex + 1) % lights.length;
+      
+      var nextLight = document.getElementById(lights[currentIndex]);
+      nextLight.classList.add(lights[currentIndex]);
+      
+      setTimeout(function() {
+        changeLights();
+      }, getDelay(currentIndex));
+    }
+    
+    function getDelay(index) {
+      if (index === 0) {
+        return 5000; // czas czerwonego światła: 5 sekund
+      } else if (index === 1) {
+        return 1000; // czas żółtego światła: 1 sekunda
+      } else if (index === 2) {
+        return 2000; // czas zielonego światła: 2 sekundy
+      }
+    }
+    
+    window.onload = function() {
+      var lights = document.getElementsByClassName('light');
+      for (var i = 0; i < lights.length; i++) {
+        lights[i].addEventListener('click', function() {
+          changeColor(this);
+        });
+      }
+      
+      changeLights();
+    };
